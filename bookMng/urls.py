16d0beb1ce8b1e39a4e11d9d151d44d1493a54a9
iGotenks <78182667+iGotenks@users.noreply.github.com>
2021-03-16 19:59:23 -0700

    Add files via upload

--- bookMng/urls.py
+++ bookMng/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('postbook', views.postbook, name='postbook'),
+    path('displaybooks', views.displaybooks, name='displaybooks'),
 ]
 
