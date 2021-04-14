50ce644d736847b40471a27891ac5dcdac7c7397
iGotenks <78182667+iGotenks@users.noreply.github.com>
2021-04-13 22:00:47 -0700

    new import, added @login_required()
    also added mybooks() and book_detail() functions

--- bookMng/views.py
+++ bookMng/views.py
@@ -14,6 +14,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request,
@@ -23,12 +24,18 @@ def index(request):
                   })
 
 
+@login_required(login_url=reverse_lazy('login'))
 def postbook(request):
     submitted = False
     if request.method == 'POST':
         form = BookForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            book = form.save(commit=False)
+            try:
+                book.username = request.user
+            except Exception:
+                pass
+            book.save()
             return HttpResponseRedirect('/postbook?submitted=True')
     else:
         form = BookForm()
@@ -43,6 +50,7 @@ def postbook(request):
                   })
 
 
+@login_required(login_url=reverse_lazy('login'))
 def displaybooks(request):
     books = Book.objects.all()
     for b in books:
@@ -52,6 +60,31 @@ def displaybooks(request):
                   {
                       'item_list': MainMenu.objects.all(),
                       'books': books
+                  })
+
+
+@login_required(login_url=reverse_lazy('login'))
+def mybooks(request):
+    books = Book.objects.filter(username=request.user)
+    for b in books:
+        b.pic_path = b.picture.url[14:]
+    return render(request,
+                  'bookMng/mybooks.html',
+                  {
+                      'item_list': MainMenu.objects.all(),
+                      'books': books
+                  })
+
+
+@login_required(login_url=reverse_lazy('login'))
+def book_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    book.pic_path = book.picture.url[14:]
+    return render(request,
+                  'bookMng/book_detail.html',
+                  {
+                      'item_list': MainMenu.objects.all(),
+                      'book': book
                   })
 
 
