1b2916537d312f83f7620842905b8b89669250e3
iGotenks <78182667+iGotenks@users.noreply.github.com>
2021-04-13 22:04:17 -0700

    2 new paths
    book_detail & mybooks

--- bookMng/urls.py
+++ bookMng/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
     path('postbook', views.postbook, name='postbook'),
+    path('mybooks', views.mybooks, name='mybooks'),
     path('displaybooks', views.displaybooks, name='displaybooks'),
 ]
 
