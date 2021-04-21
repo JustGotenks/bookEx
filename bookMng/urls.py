4d5b6de7849a2120eaaa88001fa78b199d06a937
iGotenks <78182667+iGotenks@users.noreply.github.com>
2021-04-20 19:50:31 -0700

    added book_delete path

--- bookMng/urls.py
+++ bookMng/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
+    path('book_delete/<int:book_id>', views.book_delete, name='book_delete'),
     path('postbook', views.postbook, name='postbook'),
     path('mybooks', views.mybooks, name='mybooks'),
     path('displaybooks', views.displaybooks, name='displaybooks'),
